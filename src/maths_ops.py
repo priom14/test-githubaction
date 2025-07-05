744108715236b2ca8095612582ea9e9274901911
Priom Pal <priompalnfs@yahoo.com>
2025-07-05 17:42:58 +0530

    Ops

---
+++ src/maths_ops.py
@@ -0,0 +1,5 @@
+def add(a,c):
+    return a+c
+
+def mult(a,b):
+    return a*b
---
+++ tests/test_operations.py
@@ -0,0 +1,9 @@
+from src.maths_ops import add, mult
+
+# Unit Use-Cases
+
+def test():
+    assert add(0,1)==1
+    assert add(2,3)==5
+    assert mult(0,2)==0
+    assert mult(2,3)==6
